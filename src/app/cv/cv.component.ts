df1e9433224eb97e7e8b45631dc36618eecf43f1
Kenneth <kflatner3@gmail.com>
2025-05-13 12:21:21 +0200

    changed bio

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-cv',
@@ -8,16 +7,7 @@ import html2canvas from 'html2canvas';
 })
 export class CvComponent {
 
-  downloadCV() {
-    const cvContainer = document.getElementById('cv-container');
-
-    if (cvContainer) {
-      html2canvas(cvContainer).then(canvas => {
-        const link = document.createElement('a');
-        link.download = 'cv.png';
-        link.href = canvas.toDataURL();
-        link.click();
-      });
-    }
+  printCV(): void {
+    window.print();
   }
 }
